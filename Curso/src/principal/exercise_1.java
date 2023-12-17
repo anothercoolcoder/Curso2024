960152af0e5c0273686dc84b9ee586cc95630ee8
Andres <andresalejandroninoaraujo@gmail.com>
2023-12-17 18:37:35 -0500

    Completion of the first exercise of the object oriented programming course.

---
+++ Curso/src/principal/exercise_1.java
@@ -0,0 +1,23 @@
+package principal;
+
+import java.util.Scanner;
+/* 
+Create a program to calculate the volume of a cube knowing the length of its three sides (they do not have to be equal).
+its three sides (they do not have to be equal).
+*/
+public class exercise_1 {
+    public static void main(String[] args) {
+       
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter lengths: ");
+
+        float L1 = sc.nextFloat();
+        float L2 = sc.nextFloat();
+        float L3 = sc.nextFloat();
+
+        float vol = L1 * L2 * L3;
+
+        System.out.println("The volume of the cube is: " + vol);
+    }
+}
