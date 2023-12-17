586985901cb6738bbd12f51e180a187a714b7ae9
Andres <andresalejandroninoaraujo@gmail.com>
2023-12-17 18:28:53 -0500

    primer Ejercicio

---
+++ Curso/src/principal/examplesintax.java
@@ -0,0 +1,16 @@
+package principal;
+
+import java.util.Scanner;
+
+public class examplesintax {
+    public static void main(String[] args) {
+        
+      Scanner sc = new Scanner(System.in);
+
+            System.out.println("Ingrese su nombre: ");
+            String name = sc.nextLine();
+
+            System.out.println("Hola " +name+ ", ¿como estas?");
+    }
+
+}
