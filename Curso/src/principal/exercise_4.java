2f3ca4a74ea9398672c9c542dbc1f20703ec6856
Andres <andresalejandroninoaraujo@gmail.com>
2023-12-17 19:40:12 -0500

    completion of the fourth exercise

---
+++ Curso/src/principal/exercise_4.java
@@ -0,0 +1,21 @@
+package principal;
+
+import java.util.Scanner;
+
+/*
+ Make a program that, given the ages of three people, calculates the average age.
+ */
+
+public class exercise_4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the ages of 3 people, to calculate their average: ");
+        int p1 = sc.nextInt();
+        int p2 = sc.nextInt();
+        int p3 = sc.nextInt();
+
+        int average = (p1 + p2 + p3)/3;
+        System.out.println("The average of the ages, is: " + average + ".");
+    }
+}
