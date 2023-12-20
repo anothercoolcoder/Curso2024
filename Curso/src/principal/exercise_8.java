0c6e4f1a4fad91113628100388c363748a53d024
Andres <andresalejandroninoaraujo@gmail.com>
2023-12-20 18:03:56 -0500

    10 jijijijij

---
+++ Curso/src/principal/exercise_8.java
@@ -0,0 +1,21 @@
+package principal;
+
+import java.util.Scanner;
+
+public class exercise_8 {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Dame un monto en pesos, para pasarlo a dolares, euros y bolivares: ");
+
+            double pesos = sc.nextFloat();
+            double dolar = pesos/2500;
+            double euro = pesos/3000;
+            double bolivar = pesos*0.52;
+
+            System.out.println(dolar);
+            System.out.println(euro);
+            System.out.println(bolivar);
+        }
+
+    }
+}
